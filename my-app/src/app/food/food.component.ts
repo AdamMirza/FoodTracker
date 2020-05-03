69893905e0f0064736e287211487267d0d514e3f
Adam Mirza <adammirza12@hotmail.com>
2020-05-03 13:06:41 -0700

    Move food from one list to another

--- my-app/src/app/food/food.component.ts
+++ my-app/src/app/food/food.component.ts
@@ -9,27 +9,42 @@ import { Component, OnInit } from '@angular/core';
 export class FoodComponent implements OnInit {
 
   listOfFood: Food[] = [];
+  otherListOfFood: Food[] = [];
+  globalID: number;
 
   constructor() {
+    this.globalID = 0;
   }
 
   ngOnInit() {
   }
 
   addFood() {
-    this.listOfFood.push(new Food("test", 5, "test"));
+    this.listOfFood.push(new Food(this.globalID++, "test", 5, "test"));
+  }
+
+  moveFoodToList(index: number) {
+    let temp = this.listOfFood.splice(index, 1);
+    this.otherListOfFood.push(temp[0]);
+  }
+  
+  moveFoodToOtherList(index: number) {
+    let temp = this.otherListOfFood.splice(index, 1);
+    this.listOfFood.push(temp[0]);
   }
 
 }
 
 export class Food {
   // field
+  id: number;
   name: string;
   calories: number;
   description: string;
 
   // constructor - not sure how this well translate to front-end code
-  constructor(name: string, calories: number, description: string) {
+  constructor(id: number, name: string, calories: number, description: string) {
+    this.id = id;
     this.name = name;
     this.calories = calories;
     this.description = description;
